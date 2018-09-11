3c03c1d506a4aba9e8c1a9205038ab8fc1acbf64
Liu min <781775946@qq.com>
2018-09-11 11:20:04 +0800

    1

--- ajax_express/node_express.js
+++ ajax_express/node_express.js
@@ -1,14 +1,20 @@
 var express = require('express'),
 bodyParser = require('body-parser'),
-server = express();
+server = express(),
+bodyParsers = require('./node_body_parser'),
+static = require('express-static');
 
 server.listen(9527);
 
 server.use(bodyParser.urlencoded());
 
 server.use(function(req,res){
+    
 	console.log(req.body);
+    res.send('hehehe');
 });
+
+server.use(static('./www'));
 
 // server.get('',function(req,res){
 // 	console.log('有人get了');
