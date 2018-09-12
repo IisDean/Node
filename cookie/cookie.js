9cf0c003efb7c4ad41ef16ca807d6c404793fc41
Liu min <781775946@qq.com>
2018-09-12 17:27:51 +0800

    cookie

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var static = require('express-static');
+var cookieParser = require('cookie-parser');//取cookie值插件
 
 var server = express();
 server.listen(2713);
 
-server.use('',function(req,res,next){
+server.use(cookieParser());
+
+server.use('/aaa',function(req,res,next){
+
+	//写入cookie
+	res.cookie('pass','123456232',{path: '/aaa',maxAge: 365*24*3600*1000});
+	console.log(req.cookies);
+	//path 链接地址  maxAge 失效时间
 
 	res.send('ok');
 
---
+++ cookie/cookie_demo.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+bodyParser = require('body-parser'),
+server = express(),
+static = require('express-static');
+
+server.listen(2713);
+
+server.use(bodyParser.urlencoded());
+
+server.use('',function(req,res,next){
+    res.cookie('text','hehehe',{maxAge: 365*24*3600*1000});
+    next();
+});
+
+server.use(static('./'));
