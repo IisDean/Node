13100cdf8df52b4d5439beb0c85959a8d98f4f5e
Liu min <781775946@qq.com>
2018-09-18 18:04:49 +0800

    9/18

--- jade2/jade.js
+++ jade2/jade.js
@@ -6,7 +6,32 @@ var	fs = require('fs');
 var server = express();
 server.listen(8020);
 
-server.use('/resign',function(req,res){
+server.use('/resign',resign);//注册
+server.use('/login',login);//登录
+
+server.use(static('./'));
+
+function login(req,res){
+	fs.readFile('./name.txt',function(err,data){
+		if( err ){
+			console.log('err:'+err);
+		}else{
+			var jsonData = JSON.parse(data);
+			if( jsonData[req.query.user] ){
+				if( jsonData[req.query.user] == req.query.pass ){
+					res.send('登录成功');
+				}else{
+					res.send('用户名或者密码错误');
+				}
+			}else{
+				res.send('用户名不存在');
+			}
+		}
+	});
+}
+
+//注册
+function resign(req,res){
 	fs.readFile('./name.txt',function(err,data){
 		if( err ){
 			console.log('err:'+err);
@@ -27,7 +52,4 @@ server.use('/resign',function(req,res){
 			}
 		}
 	});
-
-});
-
-server.use(static('./'));
+}
