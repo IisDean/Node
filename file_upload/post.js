c9a111692f7e2186d7ffdcf68dfc43f130621f8c
Liu min <781775946@qq.com>
2018-09-27 17:48:15 +0800

    文件上传

---
+++ file_upload/post.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+	static = require('express-static'),
+	bodyParser = require('body-parser');
+
+var server = express();
+server.listen(8020);
+
+server.post('',function(req,res){
+
+});
